2b2624921209559fb03d86887314118fe9a80428
Cameron King <cyking1233@gmail.com>
2025-03-03 04:26:38 -0500

    iframe

--- next.config.mjs
+++ next.config.mjs
@@ -6,15 +6,15 @@ const nextConfig = {
           // Apply these headers to all routes.
           source: '/(.*)',
           headers: [
-            // This sets a CSP allowing framing from your own domain and an additional allowed domain.
+            // This sets a CSP allowing framing from any domain.
             {
               key: 'Content-Security-Policy',
-              value: "frame-ancestors 'self' https://ciastudio.dev",
+              value: "frame-ancestors *",
             },
-            // Optionally, you can override the X-Frame-Options header if it’s being set elsewhere.
+            // Optionally, you can override the X-Frame-Options header if it's being set elsewhere.
             {
               key: 'X-Frame-Options',
-              value: 'ALLOW-FROM https://ciastudio.dev',
+              value: 'ALLOWALL',
             },
           ],
         },
